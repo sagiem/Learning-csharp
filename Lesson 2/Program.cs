00ffca0064cdbc1980aec35f675d53cbdb06f185
Кашин Максим Викторович <sagiems@gmail.com>
2022-04-05 12:51:56 +0600

    часы правки

--- Lesson 2/Program.cs
+++ Lesson 2/Program.cs
@@ -13,21 +13,16 @@ namespace Lesson_2
             DateTime t = DateTime.Now; 
 
             if (t.Hour >= 6 && t.Hour < 11)
-            {
                 Console.WriteLine("Доброе утро !");
-            }
+            
             else if (t.Hour >= 11 && t.Hour < 17)
-            {
                 Console.WriteLine("Добрый день !");
-            }
+            
             else if (t.Hour >= 17 && t.Hour < 23)
-            {
                 Console.WriteLine("Добрый вечер !");
-            }
+            
             else  
-            {
                 Console.WriteLine("Доброй ночи !");
-            }
 
 
             Console.ReadKey();
