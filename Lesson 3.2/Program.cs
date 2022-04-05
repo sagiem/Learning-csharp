1913a1102f4de42e4bdd0cd65f7e987ad4756154
Кашин Максим Викторович <sagiems@gmail.com>
2022-04-05 15:02:26 +0600

    деление

---
+++ Lesson 3.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10000000;
+            int[] ar = new int[size];
+            int[] ar2 = new int[size];
+            int[] InAr = new int[size];
+
+            Random random = new Random();
+
+            for (int i = 0; i < size; i++)
+                InAr[i] = random.Next();
+
+            DateTime start = DateTime.Now;
+
+            for (int i = 0; i < size; i++)
+                ar[i] = InAr[i]/2;
+            
+            TimeSpan ts = DateTime.Now - start;
+
+            start = DateTime.Now;
+
+            for (int i = 0; i < size; i++)
+                ar2[i] = InAr[i] >> 1;
+            
+            TimeSpan ts2 = DateTime.Now - start;
+
+            Console.WriteLine("Деление {0}  Сдвиг {1}", ts.Milliseconds, ts2.Milliseconds);
+
+            Console.ReadKey();
+        }
+    }
+}
