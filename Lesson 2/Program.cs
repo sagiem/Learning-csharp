22761b2fdb775128ca4aba517d8ad722a4156134
Кашин Максим Викторович <sagiems@gmail.com>
2022-04-05 11:56:45 +0600

    Часы

---
+++ Lesson 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime t = DateTime.Now; 
+
+            if (t.Hour >= 6 && t.Hour < 11)
+            {
+                Console.WriteLine("Доброе утро !");
+            }
+            else if (t.Hour >= 11 && t.Hour < 17)
+            {
+                Console.WriteLine("Добрый день !");
+            }
+            else if (t.Hour >= 17 && t.Hour < 23)
+            {
+                Console.WriteLine("Добрый вечер !");
+            }
+            else  
+            {
+                Console.WriteLine("Доброй ночи !");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
