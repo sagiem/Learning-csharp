5678b5636d7b200696f02c84c40d8c7ec270e8fd
Кашин Максим Викторович <sagiems@gmail.com>
2022-04-11 15:22:28 +0600

    строки

---
+++ Lesson 5.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String s1 = "мама мыла раму";
+            string[] arr =s1.Split(' ');
+            Array.Reverse(arr);
+            string s2 = string.Join("+", arr);
+            Console.WriteLine(s2);
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson 5.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson_5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Скоро мы пойдем домой";
+            File.WriteAllText("default.txt", s);
+            File.WriteAllText("windows1251.txt", s, Encoding.GetEncoding(1251));
+        }
+    }
+}
