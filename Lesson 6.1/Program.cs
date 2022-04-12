0708564d228b2094f1f4e9079aa702da4f57706e
Кашин Максим Викторович <sagiems@gmail.com>
2022-04-12 15:39:26 +0600

    занятие 6

---
+++ Lesson 6.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static double min2(double x, double y)
+        {
+            //return Math.Min(x, y);
+            return x < y ? x : y;
+        }
+
+        static double min4(double x1, double y1, double x2, double y2)
+        {
+
+            return min2(min2(x1, y1), min2(x2, y2));
+
+        }
+    }
+}
---
+++ Lesson 6/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 10000;
+            int max = 50000;
+            int step = 5000;
+            DateTime start;
+            TimeSpan ts1, ts2;
+
+            for (int size = min; size <= max; size += step)
+            {
+                start = DateTime.Now;
+                string s1 = GenerationString(size);
+                ts1 = DateTime.Now - start;
+
+                start = DateTime.Now;
+                string s2 = GenerationSBuilder(size);
+                ts2 = DateTime.Now - start;
+
+                Console.WriteLine("s1.lenght = {0}, s2.lenght = {1}, ts1 = {2}, ts2 = {3}, ts1/ts2 = {4}",
+                                     s1.Length,
+                                     s2.Length,
+                                     ts1.Milliseconds,
+                                     ts2.Milliseconds,
+                                     ts1.TotalMilliseconds/ts2.TotalMilliseconds);
+
+            }
+
+            Console.ReadKey();
+
+        }
+
+        static string GenerationString(int size)
+        {
+            string s = "";
+
+            for (int i = 0; i < size; i++)
+                s += string.Format("{0}", i);
+            return s;
+        }
+
+        static string GenerationSBuilder(int size)
+        {
+            
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < size; i++)
+                sb.AppendFormat("{0}", i);
+            return sb.ToString();
+        }
+    }
+}
