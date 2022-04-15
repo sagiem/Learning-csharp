b4b642cc9baeeacce186eeb7a76358436e25a1a5
Кашин Максим Викторович <sagiems@gmail.com>
2022-04-15 11:54:05 +0600

    занятие 9

---
+++ Lesson 9.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_9._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            dict["один"] = 1;
+            dict["два"] = 2;
+            dict["три"] = 3;
+            dict["четыре"] = 4;
+            dict["пять"] = 5;
+            dict["шесть"] = 6;
+            dict["семь"] = 7;
+            dict["восемь"] = 8;
+            dict["девять"] = 9;
+
+            Console.WriteLine("введите выражение типа, 'и два и три'");
+            Console.Write(">");
+            string[] patch = Console.ReadLine().Split(new string[] {" и "}, StringSplitOptions.RemoveEmptyEntries);
+            int sum = 0;
+            int value = 0;
+            bool error = false;
+
+            for (int i = 0; i < patch.Length; i++)
+            {
+                string key = patch[i];
+                if(dict.ContainsKey(key))
+                {
+                    value = dict[key];
+                    sum += value;
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестное значение ключа {0}", key);
+                    error = true;
+                    break;
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ Lesson 9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = { 2, 5, 10, 20, 9, 18, 55, 37 };
+            Array.Sort(ar,drop);
+
+            foreach (var item in ar)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+        }
+
+        static int drop(int x, int y)
+        {
+            if (x%2 != 0 && y%2 ==0)
+                return 1;
+            else
+                return -1;
+        }
+    }
+}
