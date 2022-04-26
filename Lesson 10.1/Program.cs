aff3dea0209328f7e3858fa34ca983fb9e9cf604
Кашин Максим Викторович <sagiems@gmail.com>
2022-04-26 15:35:23 +0600

    занятие 10

---
+++ Lesson 10.1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Link a = new Link();
+            a.Add(3);
+            Link b = new Link();
+            b.Add(4);
+            Link c = new Link();
+            c.Add(5);
+
+            Link.Display();
+            Console.ReadKey();
+        }
+    }
+}
+
+class Link
+{
+    private Link next;
+    private int num;
+    static Link beg;
+
+    
+    public void Add(int numb)
+    { 
+        this.num = numb; 
+        if(beg == null)
+        {
+            beg = this;
+            return;
+        }
+
+        Link temp = beg;
+        while(temp != null)
+        {
+            temp = temp.next;
+        }
+        temp.next = this;
+    }
+
+    public static void Display()
+    {
+        Link temp = beg;
+        while(temp != null)
+        {
+            Console.Write(" " + temp.next);
+            temp=temp.next;
+        }
+    }
+}
---
+++ Lesson 10.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_10._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Real number = new Real(920.5, 2);
+            Real number1 = new Real(-20.77);
+            Real number2 = new Real(number);
+        }
+    }
+
+    class Real
+    {
+        private double m = 8;
+        private int p;
+
+        public Real(double mi, int pi)
+        {
+            m = mi;
+            p = pi;
+        }
+
+        public Real(double mi)
+            :this(mi, 10){ }
+
+        public Real(Real r)
+            :this(r.m, r.p) { }
+    }
+}
